84f9af83c202e5a89dbc0d607b5cad054c68138f
Guilherme Bratiliere <guilhermebarbosa201515@gmail.com>
2025-10-20 00:21:18 -0300

    add get products

---
+++ src/actions/get-tipos/index.ts
@@ -0,0 +1,25 @@
+import { google } from "googleapis";
+
+export async function getTipos() {
+  try {
+    const keyFilePath = process.env.GOOGLE_APPLICATION_CREDENTIALS!;
+
+    const auth = new google.auth.GoogleAuth({
+      keyFile: keyFilePath,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const sheets = google.sheets({ version: "v4", auth: auth });
+
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID!,
+      range: "Sheet1!B:B",
+    });
+
+    // Retorna as linhas diretamente
+    return res.data.values;
+  } catch (error) {
+    console.error("Erro ao buscar dados da planilha:", error);
+    return [];
+  }
+}
