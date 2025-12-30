0cfe98c57ef7797f254953ff9c02808213789566
Guilherme Bratiliere <guilhermebarbosa201515@gmail.com>
2025-12-30 13:59:01 -0300

    correção no Eslint

--- src/app/detalhe/[slug]/page.tsx
+++ src/app/detalhe/[slug]/page.tsx
@@ -1,13 +1,13 @@
 import { Header } from "@/components/common/header";
 import ProductDetailClient from "./components/pageproduct";
-import React from "react";
 
 export default async function ProductPage({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
+
   return (
     <div className="bg-[#CBDCD3]">
       <Header />
@@ -15,3 +15,5 @@ export default async function ProductPage({
     </div>
   );
 }
+
+export const dynamic = "force-dynamic";
